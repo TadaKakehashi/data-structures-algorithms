5ae7e766ac12626f1f6e89de3d17013f94921b45
TadaKakehashi <aryan333498@gmail.com>
2026-03-25 21:15:14 +0530

    Move Zeroes

--- Arrays/Leetcode/Arrays2.java
+++ Arrays/Leetcode/Arrays2.java
@@ -1,5 +1,24 @@
 package Arrays.Leetcode;
 
 public class Arrays2 {
-    
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 1};  
+        Arrays2 obj = new Arrays2(); 
+        obj.moveZeroes(arr);        
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
 }
