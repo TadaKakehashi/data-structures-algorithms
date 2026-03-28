5ec56b78820e0ca242e17c6e3593bfb9cb2df209
TadaKakehashi <aryan333498@gmail.com>
2026-03-28 22:02:09 +0530

    Contains Duplicate

--- Arrays/Leetcode/Arrays4.java
+++ Arrays/Leetcode/Arrays4.java
@@ -1,7 +1,9 @@
 package Arrays.Leetcode;
 
+import java.util.Arrays;
 public class Arrays4 {
     public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
            for(int i=0;i<nums.length;i++){
                 for(int j=i+1;j<nums.length;j++){
                    if(nums[i] == nums[j]){
